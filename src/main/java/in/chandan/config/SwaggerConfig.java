a0d88f21d6364855f95694e8e64bdef8e4553f44
Chandan Kumar <43995648+nirala4evercool@users.noreply.github.com>
2023-12-05 21:38:58 +0530

    Update SwaggerConfig.java

--- src/main/java/in/chandan/config/SwaggerConfig.java
+++ src/main/java/in/chandan/config/SwaggerConfig.java
@@ -17,7 +17,7 @@ public class SwaggerConfig {
 
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("in.ashokit.rest"))
+                .apis(RequestHandlerSelectors.basePackage("in.chandan.rest"))
                 .paths(PathSelectors.any())
                 .build();
     }
